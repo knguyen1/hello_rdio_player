992f7dbd7ccd6e3133fdfb84d6b3fcbdaadb8df7
knguyen1 <yurameki@gmail.com>
2015-04-16 12:47:41 -0400

    Search capability
    Added searching and displaying results.  Still needs to get cleaned up.

--- static/js/search.js
+++ static/js/search.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     preparePage();
 
     function preparePage() {
-        $("#searchForm").on("click", function() { //bind the click button
-            $("#searchForm").submit();
+        $("#search").on("click", function() { //bind the search button to the click event
+            $("#searchForm").submit(); //make it submit the form
         })
 
-        $("#searchForm").submit(function(e) {
+        $("#searchForm").submit(function(e) { //decide what to do when the search form submits
             //do stuff
             e.preventDefault();
             performSearch();
@@ -18,6 +18,80 @@ $(document).ready(function() {
     }
 
     function performSearch() {
-        var phrase = getSearchTerm();
+
+        //get start time
+        var startTime = new Date();
+
+        //format the search term and uri and prepare for ajax
+        var searchTerm = getSearchTerm();
+        var searchUri = rdioPlayer.configs.Config.baseUri + rdioPlayer.configs.Config.searchEndpoint + encodeURIComponent(searchTerm);
+
+        //connect to the API and perform the request
+        $.ajax({
+           url: searchUri,
+           type: 'GET',
+           headers: {
+               'Accept': 'application/json'
+           },
+           async: true,
+           dataType: 'json',
+           success: function(data) {
+               displayResults(data, startTime);
+           },
+           error: function(e) {
+               //do something in error event
+           }
+        });
+    }
+
+    function displayResults(data, startTime) {
+
+        if((!data.data))
+            return false;
+
+        //artist, album, tracks
+        var searchTypes = rdioPlayer.configs.searchTypes;
+
+        var resultsContainer = document.createElement('div');
+        resultsContainer.id = "resultsContainer";
+
+        //traverse searchTypes and display them foreach searchType
+        for(var k in searchTypes)
+        {
+            var searchType = searchTypes[k];
+            var resultsDiv = document.createElement('div');
+            resultsDiv.id = searchType + "Result";
+            resultsDiv.className = "typeResult";
+            resultsDiv.innerHTML = "<h2>" + searchType + "</h2>";
+
+            //and then, each search type should have results for their type
+            //select specific type for this iteration and put it in a results array
+            var resultsThisType = data.data.filter(function (f) {
+                return f.type ==  searchType;
+            });
+
+            var resultsLength;
+            if(searchTypes[k] === searchTypes.TRACKS)
+                resultsLength = 10;
+            else
+                resultsLength = resultsThisType.length;
+
+            for(var j = 0; j < resultsLength; j++) {
+                var newP = document.createElement('p'); //put it in a 'p' item for now, for testing
+                newP.id = searchType + j;
+                newP.innerHTML = resultsThisType[j].name; //test
+                resultsDiv.appendChild(newP);
+            }
+
+            //append the searchType
+            resultsContainer.appendChild(resultsDiv);
+        }
+
+        //calculate elapsed time
+        var elapsedMs = (new Date()) - startTime;
+        //document.createElement.
+
+        //replace results in target container DOM
+        $("#results").html(resultsContainer);
     }
 });
--- static/js/utils.js
+++ static/js/utils.js
@@ -2,27 +2,47 @@
  * Created by knguyen on 4/14/2015.
  */
 
-var config = {
-    baseUri: "http://rdio-service.herokuapp.com/",
-    searchEndpoint: "/search?q=",
-    artistEndpoint: "/albums?artist_id=",
+var rdioPlayer = {
+    configs: {
 
+    }
 }
+
+rdioPlayer.configs.Config = {
+    baseUri: "https://rdio-service.herokuapp.com/",
+    searchEndpoint: "search?q=",
+    artistAlbumsEndpoint: "albums?artist_id=",
+    albumEndpoint: "albums/",
+    artistTracksEndpoint: "tracks?artist_id=",
+    tracksEndpoint: "tracks/",
+    playlistsEndpoint: "playlists/"
+}
+
+//search type enums
+//see: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/freeze
+rdioPlayer.configs.searchTypes = Object.freeze({
+  ARTISTS: "artist",
+  ALBUMS: "album",
+  TRACKS: "track"
+})
 
 function getSearchTerm() {
     var searchForm = document.forms.musicSearch;
-    var phrase = searchForm.phrase.value.trim();
+    var searchTerm = searchForm.phrase.value.trim();
 
-    if(isNullOrEmpty(phrase))
+    if(isNullOrEmpty(searchTerm))
         return "";
 
-    return phrase;
-}
-
-function getSearchUri(phrase) {
-    var baseUri = "";
+    return searchTerm;
 }
 
 function isNullOrEmpty(phrase) {
     return !phrase || phrase.length === 0;
+}
+
+function toNameCase(str) {
+    var lower = str.toLowerCase();
+    return lower.replace(/(^| )(\w)/g, function(x) {
+       return x.toUpperCase();
+    });
 }
